c0a08e85c9faa7f9456e8a37d0dc03beb36f3ae4
Miguel Dos Santos <migueldsc86@gmail.com>
2020-06-18 17:03:08 +0200

    Initial Commit

---
+++ UIViewAutolayoutExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViewAutolayoutExample
+//
+//  Created by Miguel Dos Santos Carregal on 18/06/2020.
+//  Copyright © 2020 Miguel Dos Santos Carregal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
