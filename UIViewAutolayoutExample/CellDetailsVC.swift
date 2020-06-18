16dcb750096b7d100005d3f7026e890eb67a0713
Miguel Dos Santos <migueldsc86@gmail.com>
2020-06-18 17:24:04 +0200

    start

---
+++ UIViewAutolayoutExample/CellDetailsVC.swift
@@ -0,0 +1,24 @@
+//
+//  CellDetailsVC.swift
+//  UIViewAutolayoutExample
+//
+//  Created by Miguel Dos Santos Carregal on 18/06/2020.
+//  Copyright © 2020 Miguel Dos Santos Carregal. All rights reserved.
+//
+
+import UIKit
+
+class CellDetailsVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
